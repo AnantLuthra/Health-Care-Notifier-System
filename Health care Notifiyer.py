686d11c1eab3688e6d9bd713312fd57590eb3ac5
Anant <anant312004@gmail.com>
2021-10-31 11:00:41 +0530

    Solved issues....

--- Health care Notifiyer.py
+++ Health care Notifiyer.py
@@ -1,6 +1,8 @@
 import time
 from plyer import notification
 import pyautogui
+import os
+
 def speak(str):
     '''
     Function for speaking of program.
@@ -11,7 +13,7 @@ def speak(str):
 
 def put_on_sleep(str):
     speak(str)
-    speak("Putting your laptop on sleep..")
+    speak("Putting your laptop on sleep.., because I cares about your health so much....")
     pyautogui.hotkey('win', 'd')
     pyautogui.hotkey('alt', 'f4')
     time.sleep(1)
@@ -19,39 +21,41 @@ def put_on_sleep(str):
     time.sleep(1)
     pyautogui.press("enter")
 
-def water():
+def water(path):
     time.sleep(60*20)
     speak("It's water time..")
     notification.notify(
                 title="** It's water time **",
                 message="Drink 1 glass of water from your bottle.... And enjoy programming...",
-                app_icon="C:\\Users\\star\\PycharmProjects\\Health Care notifier\\Water.ico",
+                app_icon=fr"{path}\Water.ico",
                 timeout=12)
-    put_on_sleep("First drink water and then come back..")
+    put_on_sleep("First drink 1 glass of water and then come back..")
 
-def eyes():
+def eyes(path):
     time.sleep(60*5)
     speak("It's eyes exercise time..")
     notification.notify(
                 title="** It's eye's exercise time **",
                 message="Look 20 Feet's away for next 20 seconds from now... And enjoy programming...",
-                app_icon="C:\\Users\\star\\PycharmProjects\\Health Care notifier\\Eye Exersice.ico",
+                app_icon=fr"{path}\Eye Exersice.ico",
                 timeout=12)
-    put_on_sleep("First do exercise and then come back..")
+    put_on_sleep("First do eyes exercise and then come back..")
 
-def physical():
-    time.sleep(60*3)
+def physical(path):
+    time.sleep(60*5)
     speak("It's physical exercise time..")
     notification.notify(
                 title="** It's exercise time **",
                 message="Get up from your chair and do some PT.... And enjoy programming...",
-                app_icon="C:\\Users\\star\\PycharmProjects\\Health Care notifier\\Exersice.ico",
+                app_icon=fr"{path}\Exersice.ico",
                 timeout=12)
     put_on_sleep("First do some physical activity and then come back..")
 
 if __name__ == '__main__':
+    pyautogui.hotkey('win', 'd')
     print("Welcome to Health care management...")
+    path = os.getcwd()
     while True:
-        water()
-        eyes()
-        physical()
+        water(path)
+        eyes(path)
+        physical(path)
